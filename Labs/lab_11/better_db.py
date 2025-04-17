3f3be9e6afee7f683240936e02a843fb8aba7c5a
Alikhan <alikhanmoldabaev75@gmail.com>
2025-04-17 14:36:28 +0500

    fixing some problems, lab11:done

--- Labs/lab_11/better_db.py
+++ Labs/lab_11/better_db.py
@@ -1,177 +1,125 @@
+import psycopg2
 import csv
+import ast
 
-import psycopg2 as pcg
+try:
+    connection = psycopg2.connect(
+        host='localhost', dbname="postgres", user="postgres",
+        password="1234", port=5432
+    )
 
-conn = pcg.connect(host ='localhost',dbname = "postgres", user = "postgres",
-                   password = "1234", port = 5432)
+    connection.autocommit = True  # for autocommiting all changes
 
-cur = conn.cursor()
+    curs = connection.cursor()
 
-#do smth with database
-first_start = True
-
-
-cur.execute("""CREATE TABLE IF NOT EXISTS users (
-    id INT Primary Key,
-    username VARCHAR(255),
-    user_phone VARCHAR(255)
-)
-""")
-
+    with connection.cursor() as curs:
+        curs.execute(
+            "SELECT version();"
+        )
+        print(f"server version: {curs.fetchone()}")
 
 
-while True:
+    with connection.cursor() as curs:
+        # curs.execute("DELETE FROM users WHERE username = 'username'")
+        curs.execute("SELECT * FROM users")
+        print(curs.fetchall())
 
-    if first_start:
-        first_start = False
-        print("Привет! это телефонная книга2.\n\n")
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
+
+
+def inputData() -> None:
+    print("Hello! Please, carefully input your data by following steps")
+    fname = input("Firstname: ").strip()
+    lname = input("Lastname: ").strip()
+    phone = input("Phone number: ").strip()
+    curs.execute("CALL insert_or_update_user(%s, %s, %s)", (fname, lname, phone))
+
+
+def importFromCSV() -> None:
+    with open("info.csv", 'r') as file:
+        reader = csv.reader(file)
+        li = []
+        for row in reader:
+            li.append(row)
+        curs.execute("CALL insert_multiple_users(%s)", (li,))
+
+
+def update_contact() -> bool:
+    limit = int(input("Limit: ").strip())
+    offset = int(input("Offset ").strip())
+    curs.execute('SELECT * FROM get_records_with_pagination(%s, %s);', (limit, offset))
+    print(curs.fetchall())
+
+
+def queryData() -> None:
+    print("""Select filter to querying\n\
+            [1] id, name, phone\n\
+            [2] Name, phone\n\
+            [3] Name\n\
+            [4] Phone\n\
+            [5] Exit""")
+    filter = input("Your choice: ")
+    curs.execute(' SELECT * FROM search_records_pattern(%s)', str(filter))
+    data = (curs.fetchall())
+    with open("queredData.txt", "w") as file:
+        for row in data:
+            print(row)
+            file.write(f"{str(row[0])}\n")
+
+
+def deleteData() -> None:
+    print("Enter lastname that you want to delete, then input phone number:")
+    lname = input("Lastname: ").strip()
+    phone = input("Phone number: ").strip()
+    print(f"""Are you sure to delete user {lname} from database?\n\
+        [1] - Yes\n\
+        [2] - No""")
+    sure = input()
+    if sure == "1":
+        print("Initializing deleting...")
+        curs.execute("CALL delete_records(%s, %s)", (lname, phone))
+        print("User deleted")
+
+
+def deleteAllData() -> None:
+    sure = input("Are you absolutely shure about that?: ")
+    if sure == "1":
+        print("Initializing deleting...")
+        curs.execute(' DELETE FROM users  ')
+        print("All data successfully deleted!")
     else:
-        print("\n\nВарианты действий и их коды:\n"
-              "1:Показать номера по паттерну\n"
-              "2:Добавить или обновить номер если уже есть\n"
-              "3:Добавить много номеров\n"
-              "4:Пагинация\n"
-              "5:Удалить номер по телефону.\n"
-              "6:выход\n\n" 
-              "Что вы хотите сделать ?: ",end="")
-
-        user_choice = int(input())
-
-        if user_choice == 1:
-            try:
-                print("\nВведите буквку с которой должно начинаться имя: ",end="")
-                user_letter = input()
-
-                cur.execute("""SELECT * FROM users WHERE username ILIKE %s;""", (user_letter + '%',))
-
-                rows = cur.fetchall()
-
-                for row in rows:
-                    print(row[1], row[2])
-
-                print(f"\nУспешно! были выведены все номера с именем начинающимися на букву:{user_letter}\n")
-            except Exception as e:
-                print(e)
-        elif user_choice == 2:
-            user_name = input("Введите свое имя: ")
-            user_phone = input("Введите свой номер: ")
-
-            # Проверяем, существует ли пользователь
-            cur.execute("SELECT id FROM users WHERE username = %s;", (user_name,))
-            user = cur.fetchone()
-
-            try:
-                if user:
-                    # Если пользователь уже есть, обновляем его номер
-                    cur.execute("""
-                        UPDATE users
-                        SET user_phone = %s
-                        WHERE username = %s;
-                    """, (user_phone, user_name))
-                    print("\nНомер успешно обновлён.\n")
-                else:
-                    # Если пользователя нет, добавляем его
-                    cur.execute("SELECT MAX(id) FROM users;")
-                    max_id_result = cur.fetchone()[0]
-                    last_id = 1 if max_id_result is None else max_id_result + 1
-
-                    cur.execute("""
-                        INSERT INTO users (id, username, user_phone)
-                        VALUES (%s, %s, %s);
-                    """, (last_id, user_name, user_phone))
-                    print("\nУспешно! Ваши данные добавлены в книгу.\n")
-
-                conn.commit()
-
-            except Exception as e:
-                print("Произошла ошибка:", e)
-        elif user_choice == 3:
-            times_add = int(input("Сколько добавить контактов?: "))
-
-            for time in range(times_add):
-
-                user_name = input("Введите свое имя: ")
-                user_phone = input("Введите свой номер: ")
-
-                # Проверяем, существует ли пользователь
-                cur.execute("SELECT id FROM users WHERE username = %s;", (user_name,))
-                user = cur.fetchone()
-
-                try:
-                    if user:
-                        # Если пользователь уже есть, обновляем его номер
-                        cur.execute("""
-                                        UPDATE users
-                                        SET user_phone = %s
-                                        WHERE username = %s;
-                                    """, (user_phone, user_name))
-                        print("\nНомер успешно обновлён.\n")
-                    else:
-                        # Если пользователя нет, добавляем его
-                        cur.execute("SELECT MAX(id) FROM users;")
-                        max_id_result = cur.fetchone()[0]
-                        last_id = 1 if max_id_result is None else max_id_result + 1
-
-                        cur.execute("""
-                                        INSERT INTO users (id, username, user_phone)
-                                        VALUES (%s, %s, %s);
-                                    """, (last_id, user_name, user_phone))
-                        print("\nУспешно! Ваши данные добавлены в книгу.\n")
-
-                    conn.commit()
-
-                except Exception as e:
-                    print("Произошла ошибка:", e)
-        elif user_choice == 4:
-            first_id = int(input("Введите первый айди: "))
-            second_id = int(input("Введите второй айди: "))
-
-            try:
-                cur.execute("""
-                    SELECT * FROM users
-                    WHERE id BETWEEN %s AND %s
-                    ORDER BY id;
-                """, (first_id, second_id))
-
-                rows = cur.fetchall()
-
-                if rows:
-                    print("\nНайденные пользователи:")
-                    for row in rows:
-                        print(f"ID: {row[0]}, Имя: {row[1]}, Телефон: {row[2]}")
-                else:
-                    print("\nНет пользователей в указанном диапазоне ID.")
-
-            except Exception as e:
-                print("Ошибка при выполнении запроса:", e)
+        print("Data not deleted")
 
 
+done = False
+while not done:
+    curs = connection.cursor()
+    print("What do you want to do?\n\
+          [1] Input data from console\n\
+          [2] Upload form csv file\n\
+          [3] Users by limit and offset\n\
+          [4] Query data from the table\n\
+          [5] Delete data from table by person name\n\
+          [6] Delete all data from table\n\
+          [7] Exit")
+    x = int(input("Enter number 1-7: "))
+    if (x == 1):
+        inputData()
+    elif (x == 2):
+        importFromCSV()
+    elif (x == 3):
+        if not update_contact():
+            update_contact()
 
-        elif user_choice == 5:
+    elif (x == 4):
+        queryData()
+    elif (x == 5):
+        deleteData()
+    elif (x == 6):
+        deleteAllData()
+    else:
+        done = True
 
-            try:
-                user_phone = input("\nВведите ваш номер, чтобы удалить из книги: ")
-
-                # Проверяем, существует ли номер в базе данных
-                cur.execute("""SELECT * FROM users WHERE user_phone = %s;""", (user_phone,))
-                user_exists = cur.fetchone()
-
-                if user_exists:
-                    cur.execute("""DELETE FROM users WHERE user_phone = %s;""", (user_phone,))
-                    conn.commit()
-                    print("\nУспешно! ваши данные были удалены.\n")
-                else:
-                    print("\nПользователь с таким номером не найден.\n")
-
-            except Exception as e:
-                print("Ошибка при удалении данных:", e)
-
-        if user_choice == 6:
-            break
-
-
-
-#to send stuff into databse
-conn.commit()
-cur.close()
-conn.close()
+curs.close()
+connection.close()
